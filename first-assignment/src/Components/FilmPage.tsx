bd0e38ad2f8e5d59ebec9818fd859b22922e64fc
Kelsi Kapidani <kapidani.kelsi.uni@gmail.com>
2025-04-27 21:55:57 +0200

    Fixed(FilmPage.tsx): also responsive(changed layout when in xs screen)

--- first-assignment/src/Components/FilmPage.tsx
+++ first-assignment/src/Components/FilmPage.tsx
@@ -1,17 +1,41 @@
 import { useParams } from 'react-router'
-import { Flex, Col , Row , Typography , Empty , Tag } from "antd"
+import { Flex, Col , Row , Typography , Empty , Tag , Grid } from "antd"
 import { findFilm } from '../films';
 import { useNavigate } from 'react-router'
 
+const { useBreakpoint } = Grid;
 const { Title, Paragraph } = Typography;
 
 export function FilmPage () {
 
     const navigate = useNavigate();
+    const screens = useBreakpoint();
 
     const { id } = useParams();
     const currentFilm= findFilm(Number(id));
 
+    if (screens.xs) {
+        return(
+            <>
+            {currentFilm===null ? (<Empty style={{marginTop:'50px' , marginLeft:'100px'}}/>) : (
+            <Col style={{marginLeft:'5px',marginRight:'5px'}}>
+            <Row justify='center'>
+                <img style={{width: '300px', height: 'auto'}} src={currentFilm.poster}/>
+            </Row>
+            <Row>
+            <Title>{currentFilm.name}</Title>
+            </Row>
+            <Row>
+            <Flex justify='space-around'>
+                 {currentFilm.genres.map((genre) => (<Tag style={{cursor:'pointer'}} onClick={()=>navigate(`/search?name=&genres=${genre}`)}>{genre}</Tag>))}
+            </Flex>
+            <Paragraph style={{marginTop:'20px'}}>{currentFilm.description}</Paragraph>
+            </Row>
+            </Col>
+            )}
+            </>
+        )
+    }
     return (
         <>
         {currentFilm===null ? (<Empty/>) : (
